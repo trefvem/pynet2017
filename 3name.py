101eaed477178e25c41f1a3d669aad72f7ecabbc
Ted <trefvem@aol.com>
2017-07-11 14:03:51 -0700

    String 1

---
+++ 3name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+name1 = "Ted"
+name2 = "Wai Lit"
+name3 = "Basha"
+
+name4 = raw_input("Enter fourth name: ")
+
+print("{:>30}".format(name1))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+print("{:>30}".format(name4))
+
